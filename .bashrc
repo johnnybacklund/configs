b41be6e2a1a22e9ab3ad8d92f0f686cae43508f7
Johnny <johnnybacklund@hotmail.com>
2025-01-14 21:39:51 +0100

    add nvim config

---
+++ .bashrc
@@ -0,0 +1,2 @@
+PS1='\[\e[38;5;197m\]\u\[\e[0m\] \[\e[38;5;155m\]\w\n\[\e[0m\]\$ '
+bind 'set bell-style none'
---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,8 @@
+return {
+  -- change trouble config
+  {
+    "snacks.nvim",
+    -- opts will be merged with the parent spec
+    opts = { scroll = { enabled = false } },
+  },
+}
