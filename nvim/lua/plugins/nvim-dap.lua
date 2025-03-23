036c45c3cfe9f9e04afffdc7c77d35c541a4c553
Johnny <johnnybacklund@hotmail.com>
2025-03-23 21:33:37 +0100

    Add java lsp and experiment with debugger

---
+++ nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,14 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "theHamsta/nvim-dap-virtual-text",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+    -- local dap = require("dap")
+    -- local ui = require("dapui")
+
+    -- require("dapui").setup()
+  end,
+}
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+    "nvim-java/nvim-java",
+  },
   opts = {
     inlay_hints = { enabled = false },
   },
